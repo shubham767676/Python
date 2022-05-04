c6dfcdf5e9222c12dcc4e87eeb9449cd5352def7
“shubham767676” <shubhamv.singh06@gmail.com>
2022-05-05 01:57:21 +0530

    Added: Author Information

--- Basic Core Programs/User Input.py
+++ Basic Core Programs/User Input.py
@@ -1,3 +1,9 @@
+'''
+@Author: Shubham Singh
+@Date: 05/05/2022
+@Last Modified by: Shubham Singh
+@Title : User Input and Replace String Template
+'''
 name=input("\n Enter the name: ")
 if len(name)>=3:        #Defining the minimum length
     print('\n Hello', name, end=", How are you? ") 
