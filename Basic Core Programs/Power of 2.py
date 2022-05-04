244c08302edbe196567235dd69e173a5383a3cad
“shubham767676” <shubhamv.singh06@gmail.com>
2022-05-05 02:02:52 +0530

    Added: Power of 2

---
+++ Basic Core Programs/Power of 2.py
@@ -0,0 +1,13 @@
+'''
+@Author: Shubham Singh
+@Date: 05/05/2022
+@Last Modified by: Shubham Singh
+'''
+
+print("Enter range to get power of 2")
+num=int(input())        #to get the input and cast it to int..
+n=1
+for i in range(num):
+
+    n=n*2
+    print(n)
