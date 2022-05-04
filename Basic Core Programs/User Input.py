076b1c1e68a3deb12dbe79357de7483a642c4a27
“shubham767676” <shubhamv.singh06@gmail.com>
2022-05-05 02:01:03 +0530

    Added: User Input

--- Basic Core Programs/User Input.py
+++ Basic Core Programs/User Input.py
@@ -2,7 +2,6 @@
 @Author: Shubham Singh
 @Date: 05/05/2022
 @Last Modified by: Shubham Singh
-@Title : User Input and Replace String Template
 '''
 name=input("\n Enter the name: ")
 if len(name)>=3:        #Defining the minimum length
